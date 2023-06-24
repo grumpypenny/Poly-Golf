0b1c3bd54b73b31ec65ad9a16ca3907b8711469f
grumpypenny <ajitesh.misra.14@gmail.com>
2023-06-23 21:57:07 -0400

    reconfigured how drag for power works

--- PolyGolf/Assets/Scripts 1/PlayerMovement.cs
+++ PolyGolf/Assets/Scripts 1/PlayerMovement.cs
@@ -11,14 +11,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Slider slider;
 
     // the max distance one has to drag for max power
-    [field: SerializeField] private float maxDrag = 4f;
+    [field: SerializeField] private float maxDrag = 0.25f;
+    [field: SerializeField] private float minDrag = 0.05f;
 
     private Camera cam;
     private Vector3 pointer;
 
-    // these variables are used to track mouse drag distance
-    private Vector3 downPointer;
-    private Vector3 upPointer;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,17 +38,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Charge()
     {
-        if (Input.GetMouseButtonDown(0)) 
-        {
-            upPointer = pointer;
-            downPointer = pointer;
-        }
         if (Input.GetMouseButton(0))
         {
-            upPointer = pointer;
-
-            float amount = Vector3.SqrMagnitude(upPointer - downPointer) / maxDrag;
-            slider.value = amount;
+            float amount = Vector3.SqrMagnitude(transform.position - pointer) / maxDrag;
+            if (amount > minDrag) 
+            {
+                slider.value = amount;
+            } else
+            {
+                slider.value = 0f;
+            }
         }
         if (Input.GetMouseButtonUp(0)) 
         {
