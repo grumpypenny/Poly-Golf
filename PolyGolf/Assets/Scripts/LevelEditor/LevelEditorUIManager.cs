49938fca930f43b4d8d640262f170b692b881d64
sharvenp <sharvenprasad101@gmail.com>
2023-06-25 21:57:29 -0400

    refactor level editor asset ui for better ux

--- PolyGolf/Assets/Scripts/LevelEditor/LevelEditorUIManager.cs
+++ PolyGolf/Assets/Scripts/LevelEditor/LevelEditorUIManager.cs
@@ -20,6 +20,12 @@ public class LevelEditorUIManager : MonoBehaviour
     private void Start()
     {
         levelItems = new List<LevelAssetUI>();
+        UpdateAssetView();
+    }
+
+    private void UpdateAssetView()
+    {
+        levelItems.Clear();
 
         // Clear scroll content
         foreach (Transform child in scrollViewContent.transform)
@@ -28,24 +34,22 @@ public class LevelEditorUIManager : MonoBehaviour
         }
 
         // Populate scroll content with assets
-        for (int i = 0; i < database.assets.Count; i++)  
+        for (int i = 0; i < database.assets.Count; i++)
         {
-            if (currentFilter == AssetUIFilter.All || database.assets[i].FilterTag != currentFilter)
+            if (currentFilter == AssetUIFilter.All || database.assets[i].FilterTag == currentFilter)
             {
-                // Filter data
-                continue;
+                GameObject levelItem = Instantiate(levelItemUIPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                levelItem.GetComponent<LevelAssetUI>().assetId = i;
+                levelItem.GetComponent<LevelAssetUI>().SetSprite(database.assets[i].Sprite);
+                levelItem.transform.SetParent(scrollViewContent.transform);
+                levelItems.Add(levelItem.GetComponent<LevelAssetUI>());
             }
-
-            GameObject levelItem = Instantiate(levelItemUIPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            levelItem.GetComponent<LevelAssetUI>().assetId = i;
-            levelItem.GetComponent<LevelAssetUI>().SetSprite(database.assets[i].Sprite);
-            levelItem.transform.SetParent(scrollViewContent.transform);
-            levelItems.Add(levelItem.GetComponent<LevelAssetUI>());
         }
     }
 
-    public void UpdateFilterIndex(int newIndex)
+    public void UpdateFilterIndex(int index)
     {
-        currentFilter = (AssetUIFilter)newIndex;
+        currentFilter = (AssetUIFilter)index;
+        UpdateAssetView();
     }
 }
